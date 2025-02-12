5819432e4d3d8ad98ac2c46694ce959b2f069cc6
Gustavo Stinghen <gustavobr.developer@gmail.com>
2025-02-11 21:03:46 -0300

    Conectando a AWS
    Adição da lógica de conexão ao AWS

--- REPOSITORY/AWSRepository.js
+++ REPOSITORY/AWSRepository.js
@@ -3,13 +3,14 @@ const UUID = require('uuid');
 
 const criarImagemNoBanco = (imagem) => {
     return new Promise ((resolve, reject) => {
-        const ref = UUID.v4();
+        const ref = UUID.new();
         const sql = "INSERT INTO tb_imagem_aws (referencia, idUser) VALUES (?, ? )";
         con.query(sql, [ref, imagem.idUser], (err, results) => {
             if (err) {
                 reject(new Error("Erro ao criar imagem: " + err.message));
             } else {
                 resolve(results);
+                mandarParaOAws(ref);
             }
         });
     });
@@ -27,5 +28,42 @@ const pegarImagemNoBanco = (id) => {
         });
     });
 }
+
+const mandarParaOAws = ( ref ) => {
+
+    const AWS = require('aws-sdk');
+
+    // Configuração das credenciais AWS
+    AWS.config.update({
+        region: 'us-west-1',  
+        accessKeyId: 'SEU_ACCESS_KEY',
+        secretAccessKey: 'SEU_SECRET_KEY'
+    });
+
+    const s3 = new AWS.S3();
+    const fs = require('fs');
+
+    const uploadFile = (filePath, bucketName, keyName) => {
+
+        const fileContent = fs.readFileSync(filePath);
+    
+        const params = {
+        Bucket: bucketName,  // Nome do seu bucket S3
+        Key: keyName,        // Nome do arquivo no S3
+        Body: fileContent    // Conteúdo do arquivo
+        };
+    
+        s3.upload(params, (err, data) => {
+            if (err) {
+                console.error('Erro ao fazer o upload:', err);
+            } else {
+                console.log('Arquivo carregado com sucesso:', data.Location);
+            }
+        });
+    };
+
+    uploadFile ( '/IMAGENS/starwars.jpg', 'bucketmi74', ref );
+}
+
 
 module.exports = { criarImagemNoBanco, pegarImagemNoBanco };
